74f3b744c9f0083fd0fd221ec18253ce4b03ff18
yuefunglee <yuefunglee@cdnis.edu.hk>
2020-06-12 15:04:37 +0800

    Initial Commit

---
+++ Overtime/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Overtime
+//
+//  Created by Yue Fung Lee on 12/6/2020.
+//  Copyright © 2020 Yue Fung Lee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
